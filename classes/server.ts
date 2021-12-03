21f9df1ee4b25e80e3a083195f9a456a8156afea
rebeco <rebeco@gmail.com>
2021-12-03 11:17:13 +0100

    feat: Añadido socket.io server

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,43 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server {
+  private static _instance: Server;
   public app: express.Application;
   public port: number;
 
-  constructor() {
+  public io: socketIO.Server;
+  private httpServer: http.Server;
+
+  private constructor() {
     this.app = express();
     this.port = SERVER_PORT;
+
+    this.httpServer = new http.Server(this.app);
+    this.io = new socketIO.Server(this.httpServer, {
+      cors: {
+        origin: true,
+        credentials: true,
+      },
+    });
+
+    this.escucharSockets();
+  }
+
+  public static get instance() {
+    return this._instance || (this._instance = new this());
+  }
+
+  private escucharSockets() {
+    console.log('Escuchando sockets...');
+    this.io.on('connection', (cliente) => {
+      console.log('Nuevo cliente');
+    });
   }
 
   start(callback: any) {
-    this.app.listen(this.port, callback);
+    this.httpServer.listen(this.port, callback);
   }
 }
